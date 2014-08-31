b0eb58e5979df14997ecaff394b7f18bd28f9067
Filip Pacurar <hello@filipac.net>
2014-08-31 16:40:28 +0300

    working analytics events

--- src/Djemba/Embeder.php
+++ src/Djemba/Embeder.php
@@ -61,7 +61,7 @@ class Embeder
 
   ga('create', '{$this->analyticsId}', 'auto');
   ga('send', 'pageview');
-  ga('send','event','LiveHistory','Visitors','Ip','{$ip}');
+  ga('send','event','{$ip}','Ips');
   setInterval(function() {
     ga('send', 'pageview');
   }, 60000);
